59e53b4042752083df5c0a5b1992b60a7e038cff
tanushreelokesh12 <84696879+tanushreelokesh12@users.noreply.github.com>
2022-12-13 14:04:35 +0530

    Add files via upload

---
+++ is_sorted.py
@@ -0,0 +1,7 @@
+def is_sorted(t):
+    for index, item in enumerate(t):
+        try:
+            if item > t[index + 1]:
+                return False
+        except IndexError:
+            return True
