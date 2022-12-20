0d4fb6ccaf2be2fcdd8134daa4b9ca483163772a
tanushreelokesh12 <84696879+tanushreelokesh12@users.noreply.github.com>
2022-12-20 19:06:26 +0530

    Add files via upload

---
+++ basic salary.py
@@ -0,0 +1,7 @@
+basic_sal=25000
+profit=200
+com_rate=0.02
+numberofcamera=int(input("enter the number of cameras sold:"))
+price=float(input("enter the total price of camera:"))
+bonus=(profit*numberofcamera)
+print("bonus:",bonus)
