65d58689a20a48f983a15ab8a740520b3b1ec938
tanushreelokesh12 <84696879+tanushreelokesh12@users.noreply.github.com>
2022-12-20 19:26:09 +0530

    Add files via upload

---
+++ feet to inches.py
@@ -0,0 +1,6 @@
+a= int(input(" enter height in inches: "))
+b= a*2.54
+print("the conversion of inch to cm is : " +str(b))
+c= int(input(" enter height in feet: "))
+d=c*12
+print(" the conversion of feet to inches is : " +str(d))
